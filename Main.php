9b9b50909351a894921455af738783c35b12554d
usy4 <68358524+usy4@users.noreply.github.com>
2022-11-22 12:37:28 +0300

    Create Main.php

---
+++ Main.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace usy4\SwapBall;
+
+use usy4\SwapBall\commands\SwapBallCommand;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\item\VanillaItems;
+use pocketmine\event\Listener;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable() : void{
+
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getCommandMap()->register($this->getName(), new SitWandsCommand($this));        
+    
+    }
+
+	public function addWands(Player $player){
+		$item = VanillaItems::SNOWBALL();
+		$item->setCustomName("§r§cSwap§cBall\n§7Shoot a player");
+		$player->getInventory()->addItem($item1);
+                $player->sendMessage("Done.");
+	}
+
+    
+	public function onDamage(EntityDamageEvent $event): void{
+		$entity = $event->getEntity();
+		if($entity instanceof Player){
+			if($event instanceof EntityDamageByEntityEvent && ($damager = $event->getDamager()) instanceof Player){
+				$item = $damager->getInventory()->getItemInHand()->getName();
+				if($item == "§r§cSwap§cBall\n§7Shoot a player"p){
+					$event->cancel();
+					$this->onHit($damager, $entity);
+				}	
+			}
+		}
+	}
+
+	public function onHit(Player $subject, Player $targetPlayer) {
+		if($subject->getName() === $subject->getName()) 
+			return;			
+   		$dl = $subject->getLocation();
+                $el = $targetPlayer->getLocation();
+                $subject->teleport($el);
+                $targetPlayer->teleport($dl);
+                $subject->sendMessage("§bYou swapped with §r'.$targetPlayer->getName());
+	   	$targetPlayer->sendMessage('§bYou swapped with §r'.$subject->getName());
+	}
+
+}
