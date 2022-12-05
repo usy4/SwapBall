8b0df30e3bbc7393059dae77969907b0eb4832a9
usy4 <68358524+usy4@users.noreply.github.com>
2022-12-05 21:43:41 +0300

    Update Main.php

--- src/usy4/SwapBall/Main.php
+++ src/usy4/SwapBall/Main.php
@@ -18,61 +18,65 @@ use pocketmine\event\player\PlayerItemUseEvent;
 
 class Main extends PluginBase implements Listener{
 
-    public function onEnable() : void{
-	$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	$this->getServer()->getCommandMap()->register($this->getName(), new SwapBallCommand($this));        
-    }
+
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getCommandMap()->register($this->getName(), new SwapBallCommand($this));        
+	}
 	
-    public function addSwapBall(Player $player, $amount){
-	$item = VanillaItems::SNOWBALL()->setCount($amount);
-	$item->setCustomName("§r§cSwap§bBall\n§7Shoot a player");
-	$player->getInventory()->addItem($item);
-        $player->sendMessage("Done.");	
-    }
     
-    public function onLaunch(ProjectileLaunchEvent $event){  
-        $entity = $event->getEntity();
-        $owner = $entity->getOwningEntity();
-        if($owner === null) return;
-	if($owner instanceof Player){
-	    if($owner->getInventory()->getItemInHand()->getName() == "§r§cSwap§bBall\n§7Shoot a player"){
-		$entity->setNameTag("SwapBall");  
-	    }
-        }
-    }
+	public function addSwapBall(Player $player, $amount){
+		$item = VanillaItems::SNOWBALL()->setCount($amount);
+		$item->setCustomName("§r§cSwap§bBall\n§7Shoot a player");
+		$player->getInventory()->addItem($item);
+		$player->sendMessage("Done.");	
+    	}
     
-    /**
+	public function onLaunch(ProjectileLaunchEvent $event){  
+		$entity = $event->getEntity();
+		$owner = $entity->getOwningEntity();
+		if($owner !== null) {
+			if($owner instanceof Player){
+				if($owner->getInventory()->getItemInHand()->getName() == "§r§cSwap§bBall\n§7Shoot a player"){
+					$entity->setNameTag("SwapBall");  
+				}
+			}
+		}
+	}
+    
+	/**
        * @ignoreCancelled true
        * @priority MONITOR
-         */
+      */
     
-    public function onHit(ProjectileHitEvent $event) : void{
-        $entity = $event->getEntity();
-        $owner = $entity->getOwningEntity();
-        if($owner === null) return;
-        $et = $entity->getNameTag();	 
-        if($event instanceof ProjectileHitEntityEvent && ($target = $event->getEntityHit()) instanceof Player){         
-            if($et == "SwapBall"){
-                $this->Hit($owner, $target);
-            }		
-        }
-    }
+	public function onHit(ProjectileHitEvent $event) : void{
+		$entity = $event->getEntity();
+		$owner = $entity->getOwningEntity();
+		if($owner === null) {
+			$et = $entity->getNameTag();	 
+			if($event instanceof ProjectileHitEntityEvent && ($target = $event->getEntityHit()) instanceof Player){         
+				if($et == "SwapBall"){
+					$this->Hit($owner, $target);
+				}		
+			}
+		}
+	}
     
-    public function Hit(Player $subject, Player $targetPlayer) {
-	if($subject->getName() === $targetPlayer->getName()) 
-		return;			
-   	$dl = $subject->getLocation();
-        $el = $targetPlayer->getLocation();
-        $subject->teleport($el);
-        $targetPlayer->teleport($dl);
-        $subject->sendMessage('§bYou swapped with §r'.$targetPlayer->getName());
-	$targetPlayer->sendMessage('§bYou swapped with §r'.$subject->getName());
-    }
+	public function Hit(Player $subject, Player $targetPlayer) {
+		if($subject->getName() === $targetPlayer->getName()) 
+			return;			
+		$dl = $subject->getLocation();
+	        $el = $targetPlayer->getLocation();
+		$subject->teleport($el);
+        	$targetPlayer->teleport($dl);
+        	$subject->sendMessage('§bYou swapped with §r'.$targetPlayer->getName());
+		$targetPlayer->sendMessage('§bYou swapped with §r'.$subject->getName());
+	}
     
-    public function onChild(EntityDamageByChildEntityEvent $event){
-        if($event->getChild()->getNameTag() == "SwapBall"){
-            $event->cancel();
-        }
-    }
+	public function onChild(EntityDamageByChildEntityEvent $event){
+		if($event->getChild()->getNameTag() == "SwapBall"){
+			$event->cancel();
+		}
+	}
         
 }
